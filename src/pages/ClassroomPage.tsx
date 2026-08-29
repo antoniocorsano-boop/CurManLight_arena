3129dfb875879141dc5116f5e1f35a479aca6bb8
Antonio Corsano <antonio.corsano@gmail.com>
2026-08-29 18:16:34 +0200

    S2B-3B remove retired Classroom page wrapper

--- src/pages/ClassroomPage.tsx
+++
@@ -1,7 +0,0 @@
-import { useAppContext } from '../components/layout/AppContext';
-import { ClasseTab } from '../features/classroom';
-
-export default function ClassroomPage() {
- const ctx = useAppContext();
- return <ClasseTab {...ctx} />;
-}
