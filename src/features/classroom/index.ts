9011114608c6955bc734fc9575703399365d205a
Antonio Corsano <antonio.corsano@gmail.com>
2026-08-29 18:16:47 +0200

    S2B-3B stop exporting retired Classroom UI

--- src/features/classroom/index.ts
+++ src/features/classroom/index.ts
@@ -1,4 +1,2 @@
-export { ClasseTab } from './components';
-export type { ClasseTabProps } from './components';
 export { useClassroomSocialHandlers } from './hooks/useClassroomSocialHandlers';
 export { useClassroomSocialState } from './hooks/useClassroomSocialState';
