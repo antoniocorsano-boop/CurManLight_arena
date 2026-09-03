6e586917c54edc49c18cee440fe3603e395f8a29
Antonio Corsano <antonio.corsano@gmail.com>
2026-09-03 22:11:29 +0200

    feat(r7c6c): route Fonti through preflight workspace

--- src/features/documents/components/index.ts
+++ src/features/documents/components/index.ts
@@ -1,6 +1,6 @@
 export { default as SecondBrainTab } from './SecondBrainTab';
 export { EsportazioniTab } from './EsportazioniTab';
-export { FontiTab } from './FontiTab';
+export { FontiWorkspace as FontiTab } from './FontiWorkspace';
 export { CanonicalDocumentTab } from './CanonicalDocumentTab';
 export { AddKbDocumentModal, WikiReaderModal } from './KnowledgeModals';
 
