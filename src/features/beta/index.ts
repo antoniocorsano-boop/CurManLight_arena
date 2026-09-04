5580963a14ca56501f7f358a43437ba2f042ddd5
Antonio Corsano <antonio.corsano@gmail.com>
2026-09-04 10:38:28 +0200

    feat(arena): export team meeting workspace

--- src/features/beta/index.ts
+++ src/features/beta/index.ts
@@ -1,3 +1,4 @@
 export { default as BetaIdentityPage } from './BetaIdentityPage';
 export { InstitutionalDecisionPanel } from './InstitutionalDecisionPanel';
 export { StructuredProposalStarter } from './StructuredProposalStarter';
+export { TeamReviewWorkspace } from './TeamReviewWorkspace';
