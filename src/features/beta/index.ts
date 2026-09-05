f91df60ca4db3382d16e5c06c6ec8ee973a6c8b4
Antonio Corsano <antonio.corsano@gmail.com>
2026-09-05 16:39:05 +0200

    feat(arena): export revision workspace components

--- src/features/beta/index.ts
+++ src/features/beta/index.ts
@@ -2,3 +2,6 @@ export { default as BetaIdentityPage } from './BetaIdentityPage';
 export { InstitutionalDecisionPanel } from './InstitutionalDecisionPanel';
 export { StructuredProposalStarter } from './StructuredProposalStarter';
 export { TeamReviewWorkspace } from './TeamReviewWorkspace';
+export { TeamContributionPublisher } from './TeamContributionPublisher';
+export { TeamCoordinationWorkspace } from './TeamCoordinationWorkspace';
+export { RevisionWorkspace } from './RevisionWorkspace';
