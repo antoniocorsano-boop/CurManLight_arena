76f21c3f6a98cdc293a00be152fca4c4034a3fc5
Antonio Corsano <antonio.corsano@gmail.com>
2026-09-07 06:17:34 +0200

    Arena: rende dominante la sessione case-scoped nel Riesame

---
+++ src/features/beta/CaseAwareRevisionSurface.tsx
@@ -0,0 +1,56 @@
+import { useMemo } from 'react';
+import { resolveCurriculumUnitReference } from '../../domain/curriculum/didacticBinding';
+import { reviewCaseMatchesCurrentUnit } from '../../domain/curriculum/reviewCase';
+import { useCurriculumStore } from '../../store/useCurriculumStore';
+import type { AppViewsLayerProps } from '../session/types/appViewContracts';
+import { CaseScopedCurriculumWorkSession } from './CaseScopedCurriculumWorkSession';
+import { RevisionWorkspace } from './RevisionWorkspace';
+
+type Props = AppViewsLayerProps & {
+  initialNormativeSourceCode?: string | null;
+  onInitialNormativeSourceConsumed?: () => void;
+};
+
+export function CaseAwareRevisionSurface(props: Props) {
+  const curriculumReviewCases = useCurriculumStore((state) => state.curriculumReviewCases ?? []);
+  const schoolYear = useCurriculumStore((state) => state.schoolYear);
+  const currentUnit = useMemo(() => resolveCurriculumUnitReference({
+    order: props.order,
+    targetClass: props.targetClass,
+    disciplineOrField: props.discipline,
+  }), [props.discipline, props.order, props.targetClass]);
+
+  const activeCase = useMemo(() => curriculumReviewCases.find((reviewCase) => (
+    reviewCase.workSession?.sessionState === 'ACTIVE'
+    && reviewCase.caseState === 'PROFESSIONAL_VALIDATION_IN_PROGRESS'
+    && reviewCase.currentHumanPhase === 'H2_PROFESSIONAL_VALIDATION'
+    && reviewCaseMatchesCurrentUnit(reviewCase, currentUnit)
+  )) ?? null, [curriculumReviewCases, currentUnit]);
+
+  if (activeCase) {
+    return (
+      <div
+        data-revision-surface-mode="CASE_SCOPED"
+        data-active-curriculum-review-case={activeCase.id}
+      >
+        <CaseScopedCurriculumWorkSession
+          reviewCase={activeCase}
+          availableProposals={props.currentDisciplineProps}
+          discipline={props.discipline}
+          order={props.order}
+          academicYear={schoolYear}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div data-revision-surface-mode="GENERAL">
+      <RevisionWorkspace
+        {...props}
+        initialNormativeSourceCode={props.initialNormativeSourceCode}
+        onInitialNormativeSourceConsumed={props.onInitialNormativeSourceConsumed}
+      />
+    </div>
+  );
+}
